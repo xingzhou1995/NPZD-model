8ef9d9ecb2fc617f0f7273502c398d705d73b23d
Xing ZHou <@mtu.edu>
2018-02-09 14:45:53 -0500

    Adding sinking and diffusion

---
+++ P-model/NPZD-model-P/mixing.f90
@@ -0,0 +1,150 @@
+program mixing
+implicit none
+integer :: i,j
+integer :: n
+real :: dh,dt,A,S
+real(kind=8),allocatable ::  BELOW(:)
+real(kind=8),allocatable ::  MAIN(:)
+real(kind=8),allocatable ::  ABOVE(:)
+real(kind=8),allocatable ::  N1(:)
+real(kind=8),allocatable ::  N2(:)
+!intial value
+n=20
+
+allocate(N1(n))
+allocate(N2(n))
+allocate(BELOW(n))
+allocate(MAIN(n))
+allocate(ABOVE(n))
+
+do i=1,n
+
+N1(i)=21-i
+
+end do
+
+!N1(1)=1.9090909090909
+!N1(2)=2.3636363636363
+!N1(3)=3.0000000000000
+!N1(4)=3.3636363636363
+!N1(5)=4.0909090909090
+
+
+dh = 1 !space step
+dt = 1 !time step
+A  = 2 ! diffuse coefficient
+S  = A*((dt)/(dh*dh))
+
+BELOW(1)= 0
+BELOW(n)= (-1)*S
+ABOVE(1) = (-1)*S
+!ABOVE(1) = S
+ABOVE(n) = 0
+MAIN(1) = 1+S
+MAIN(n) = 1+S 
+!MAIN(1)=1-S
+
+
+
+do i=2,n-1
+BELOW(i) = (-1)*S
+MAIN(i)  = 1+2*S
+ABOVE(i) = (-1)*S 
+end do
+
+!write(*,*) BELOW
+!write(*,*) MAIN
+!write(*,*) ABOVE
+
+do i=1,100
+call solve_tradiag(BELOW,MAIN,ABOVE,N1,N2,n)
+
+  do j=1,n
+  N1(j)=N2(j)
+  end do
+  write(*,*) i,N1
+end do
+
+!write(*,*) N2
+
+end
+
+ 
+
+subroutine solve_tradiag(a,bb,c,d,x,n)
+implicit none
+! a - sub-diagonal below the main diagonal
+! b - the main diagonal
+! c - suo-diagonal
+! d - right part
+! x - the answer
+! n -number of equations
+
+integer :: n
+real(kind=8),dimension(n) :: a,b,c,d,bb
+real(kind=8),dimension(n) :: x
+!real(kind=8),dimension(n) :: cp,dp
+!real(8) :: m
+integer i
+
+do i=1,n
+b(i)=bb(i)
+end do
+
+
+
+
+!write(*,*) "BELOW=",a
+!write(*,*) "MAIN=",b
+!write(*,*) "ABOVE=", c
+!write(*,*) "N1=",d
+
+
+
+
+
+
+! initialize c-prime and d-prime
+!cp(1) = c(1)/b(1)
+!dp(1) = d(1)/b(1)
+! solve vectors c-prime and d-prime
+do i=2,n
+ !write(*,*) "b(i)=",b(i)
+ !write(*,*) "a(i)=",a(i)
+ !write(*,*) "c(i-1)=",c(i-1)
+ !write(*,*) "d(i)=",d(i-1),d(i)
+ b(i)=b(i)-((a(i)/b(i-1))*c(i-1))
+ d(i)=d(i)-((a(i)/b(i-1))*d(i-1))
+  
+!write(*,*) "i=",i,"b=",b(i)
+!write(*,*) "i=",i,"d=",d(i)
+end do
+
+
+!do i= 2,n
+! m=b(i)-cp(i-1)*a(i)
+! cp(i)=c(i)/m
+! dp(i)=(d(i)-dp(i-1)*a(i))/m
+!end do
+
+!intialize x
+x(n)=d(n)/b(n)
+
+!write(*,*) x(n)
+
+!solve for x from the vectors c-prime and d-prime
+
+do i=n-1,1,-1
+   x(i)=(d(i)-(c(i)*x(i+1)))/b(i)
+
+
+! do i = n-1,1,-1
+! x(i)=dp(i)-cp(i)*x(i+1)
+ end do
+
+!write(*,*) x
+
+end subroutine solve_tradiag
+
+
+
--- P-model/NPZD-model-P/phy_process.f90
+++ P-model/NPZD-model-P/phy_process.f90
@@ -35,29 +35,38 @@ subroutine sinking(time)
 use NPZD_input
 use phy_parameter
 implicit none
-integer :: j,time,sink_controln,sink_controlp,sink_controlz,sink_controld
+integer :: j,time
+!,sink_controln,sink_controlp,sink_controlz,sink_controld
 real(kind=8) :: N1(LAYER),P1(LAYER),Z1(LAYER),D1(LAYER)
 real(kind=8) :: N2(LAYER),P2(LAYER),Z2(LAYER),D2(LAYER)
-real(kind=8) :: N3(LAYER),P3(LAYER),Z3(LAYER),D3(LAYER)
+!real(kind=8) :: N3(LAYER),P3(LAYER),Z3(LAYER),D3(LAYER)
 
-sink_controln = 1
-sink_controlp = 1
-sink_controlz = 1
-sink_controld = 1
+!sink_controln = 1
+!sink_controlp = 1
+!sink_controlz = 1
+!sink_controld = 1
 
 
 
 !read data
 do j=1,LAYER
-N1(j)=array_N(time-1,j)
-N2(j)=array_N(time,j)
-P1(j)=array_P(time-1,j)
-P2(j)=array_P(time,j)
-Z1(j)=array_Z(time-1,j)
-Z2(j)=array_Z(time,j)
-D1(j)=array_D(time-1,j)
-D2(j)=array_D(time,j)
+!N1(j)=array_N(time-1,j)
+N1(j)=array_N(time,j)
+!P1(j)=array_P(time-1,j)
+P1(j)=array_P(time,j)
+!Z1(j)=array_Z(time-1,j)
+Z1(j)=array_Z(time,j)
+!D1(j)=array_D(time-1,j)
+D1(j)=array_D(time,j)
 end do
+
+do j=1,LAYER
+N2(j)=0
+P2(j)=0
+Z2(j)=0
+D2(j)=0
+end do
+
 !#########################################################
 
 !write(*,*) "Z2=",Z2
@@ -65,20 +74,26 @@ end do
 !write(*,*) "Z1=",Z1
 !write(*,*)  sink_controlz
 !write(*,*)  Wz
-N3(1)=N1(1)-sink_controln*Wn*(dt/dh)*((N2(1)+N2(2))/2)
-P3(1)=P1(1)-sink_controlp*Wp*(dt/dh)*((P2(1)+P2(2))/2)
-Z3(1)=Z1(1)-sink_controlz*Wz*(dt/dh)*((Z2(1)+Z2(2))/2)
-D3(1)=D1(1)-sink_controld*Wd*(dt/dh)*((D2(1)+D2(2))/2)
+!N2(1)=N1(1)-Wn*(dt/dh)*((N2(1)+N2(2))/2)
+!P3(1)=P2(1)-sink_controlp*Wp*(dt/dh)*((P2(1)+P2(2))/2)
+!Z3(1)=Z2(1)-sink_controlz*Wz*(dt/dh)*((Z2(1)+Z2(2))/2)
+!D3(1)=D2(1)-sink_controld*Wd*(dt/dh)*((D2(1)+D2(2))/2)
+!call sink_control(N3(1),P3(1),Z3(1),D3(1),sink_controln,sink_controlp,sink_controlz,sink_controld)
 
-call sink_control(N3(1),P3(1),Z3(1),D3(1),sink_controln,sink_controlp,sink_controlz,sink_controld)
 
+
+!upwind scheme
+N2(1)=N1(1)-Wn*(dt/dh)*(N1(1)-0)
+P2(1)=P1(1)-Wp*(dt/dh)*(P1(1)-0)
+Z2(1)=Z1(1)-Wz*(dt/dh)*(Z1(1)-0)
+D2(1)=D1(1)-Wd*(dt/dh)*(D1(1)-0)
 
 Write(*,*),"#############SINKING##################"
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"N=",N3(1)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"P=",P3(1)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"Z=",Z3(1)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"D=",D3(1)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"SUM=",N3(1)+P3(1)+Z3(1)+D3(1)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"N=",N2(1)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"P=",P2(1)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"Z=",Z2(1)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"D=",D2(1)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",1,"SUM=",N2(1)+P2(1)+Z2(1)+D2(1)
 write(*,*),"#############SINKING##################"
 
 
@@ -86,28 +101,35 @@ write(*,*),"#############SINKING##################"
 
 
 do j=2,LAYER-1
-N3(j)=N1(j)-sink_controln*Wn*(dt/dh)*(N2(j+1)-N2(j-1))
-P3(j)=P1(j)-sink_controlp*Wp*(dt/dh)*(P2(j+1)-P2(j-1))
-Z3(j)=Z1(j)-sink_controlz*Wz*(dt/dh)*(Z2(j+1)-Z2(j-1))
-D3(j)=D1(j)-sink_controld*Wd*(dt/dh)*(D2(j+1)-D2(j-1))
+!N3(j)=N1(j)-sink_controln*Wn*(dt/dh)*(N2(j+1)-N2(j-1))
+!P3(j)=P1(j)-sink_controlp*Wp*(dt/dh)*(P2(j+1)-P2(j-1))
+!Z3(j)=Z1(j)-sink_controlz*Wz*(dt/dh)*(Z2(j+1)-Z2(j-1))
+!D3(j)=D1(j)-sink_controld*Wd*(dt/dh)*(D2(j+1)-D2(j-1))
 
-call sink_control(N3(j),P3(j),Z3(j),D3(j),sink_controln,sink_controlp,sink_controlz,sink_controld)
+!call sink_control(N3(j),P3(j),Z3(j),D3(j),sink_controln,sink_controlp,sink_controlz,sink_controld)
+
+N2(j)=N1(j)-Wn*(dt/dh)*(N1(j)-N1(j-1))
+P2(j)=P1(j)-Wp*(dt/dh)*(P1(j)-P1(j-1))
+Z2(j)=Z1(j)-Wz*(dt/dh)*(Z1(j)-Z1(j-1))
+D2(j)=D1(j)-Wd*(dt/dh)*(D1(j)-D1(j-1))
+
 
 Write(*,*),"#############SINKING##################"
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"N=",N3(j)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"P=",P3(j)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"Z=",Z3(j)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"D=",D3(j)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"SUM=",N3(j)+P3(j)+Z3(j)+D3(j)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"N=",N2(j)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"P=",P2(j)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"Z=",Z2(j)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"D=",D2(j)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",j,"SUM=",N2(j)+P2(j)+Z2(j)+D2(j)
 write(*,*),"#############SINKING##################"
 
 
 
 end do
-N3(LAYER)=N1(LAYER)+sink_controln*Wn*(dt/dh)*((N2(LAYER)+N2(LAYER-1))/2)
-P3(LAYER)=P1(LAYER)+sink_controlp*Wp*(dt/dh)*((P2(LAYER)+P2(LAYER-1))/2)
-Z3(LAYER)=Z1(LAYER)+sink_controlz*Wz*(dt/dh)*((Z2(LAYER)+Z2(LAYER-1))/2)
-D3(LAYER)=D1(LAYER)+sink_controld*Wd*(dt/dh)*((D2(LAYER)+D2(LAYER-1))/2)
+
+!N(LAYER)=N1(LAYER)+sink_controln*Wn*(dt/dh)*((N2(LAYER)+N2(LAYER-1))/2)
+!3(LAYER)=P1(LAYER)+sink_controlp*Wp*(dt/dh)*((P2(LAYER)+P2(LAYER-1))/2)
+!3(LAYER)=Z1(LAYER)+sink_controlz*Wz*(dt/dh)*((Z2(LAYER)+Z2(LAYER-1))/2)
+!3(LAYER)=D1(LAYER)+sink_controld*Wd*(dt/dh)*((D2(LAYER)+D2(LAYER-1))/2)
 
 !write(*,*) "Z3(LAYER)=",time,Z3(LAYER)
 !write(*,*) "Z1(LAYER)=",time,Z1(LAYER)
@@ -116,19 +138,32 @@ D3(LAYER)=D1(LAYER)+sink_controld*Wd*(dt/dh)*((D2(LAYER)+D2(LAYER-1))/2)
 !do j=1,LAYER
 !write(*,*) time,Z3(j)
 !end do
-call sink_control(N3(LAYER),P3(LAYER),Z3(LAYER),D3(LAYER),sink_controln,sink_controlp,sink_controlz,sink_controld)
+!call sink_control(N3(LAYER),P3(LAYER),Z3(LAYER),D3(LAYER),sink_controln,sink_controlp,sink_controlz,sink_controld)
 !do j=1,LAYER
 !write(*,*) time,Z3(j)
 !end do
 
+!upwind scheme
+
+
+N2(LAYER)=sum(N1)-sum(N2)
+
+P2(LAYER)=sum(P1)-sum(P2)
+
+Z2(LAYER)=sum(Z1)-sum(Z2)
+
+D2(LAYER)=sum(D1)-sum(D2)
+
+
 Write(*,*),"#############SINKING##################"
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"N=",N3(LAYER)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"P=",P3(LAYER)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"Z=",Z3(LAYER)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"D=",D3(LAYER)
-Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"SUM=",N3(LAYER)+P3(LAYER)+Z3(LAYER)+D3(LAYER)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"N=",N2(LAYER)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"P=",P2(LAYER)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"Z=",Z2(LAYER)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"D=",D2(LAYER)
+Write(*,*),"TIME=",TSTART+(time-1)*dt,"LAYER=",LAYER,"SUM=",N2(LAYER)+P2(LAYER)+Z2(LAYER)+D2(LAYER)
 write(*,*),"#############SINKING##################"
 
+write(*,*),"SUM=",sum(N2)+sum(P2)+sum(Z2)+sum(D2)
 
 !do j=1,LAYER
 
@@ -161,10 +196,10 @@ write(*,*),"#############SINKING##################"
 !write(*,*) "Z3=",Z3
 do j=1,LAYER
 
-   array_N(time+1,j)=N3(j)
-   array_P(time+1,j)=P3(j)
-   array_Z(time+1,j)=Z3(j)
-   array_D(time+1,j)=D3(j)
+   array_N(time+1,j)=N2(j)
+   array_P(time+1,j)=P2(j)
+   array_Z(time+1,j)=Z2(j)
+   array_D(time+1,j)=D2(j)
 end do
 
 
